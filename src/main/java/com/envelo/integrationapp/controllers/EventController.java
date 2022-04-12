294332362b82a13e958ce97fa4af15bfe5804213
Kiryl Zhuk <kizhofficial@gmail.com>
2022-04-13 00:06:32 +0200

    Add endpoint

--- src/main/java/com/envelo/integrationapp/controllers/EventController.java
+++ src/main/java/com/envelo/integrationapp/controllers/EventController.java
@@ -1,10 +1,21 @@
 package com.envelo.integrationapp.controllers;
 
 import com.envelo.integrationapp.model.dtos.EventCreationDto;
+import com.envelo.integrationapp.model.dtos.info.EventDtoInfo;
 import com.envelo.integrationapp.model.entities.Event;
+import com.envelo.integrationapp.model.enums.EventRole;
 import com.envelo.integrationapp.services.EventService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.beans.support.PagedListHolder.DEFAULT_PAGE_SIZE;
 
 @RestController
 @RequestMapping("/events")
@@ -22,6 +33,9 @@ public class EventController {
     public Event addEvent(@RequestBody EventCreationDto eventCreationDto) {
         return eventService.addEvent(eventCreationDto);
     }
+
     @GetMapping
-    public List<Event> get
+    public List<EventDtoInfo> getCreatedEvents() {
+            return eventService.getAllUserCreatedEvents(1L, EventRole.HOST);
+    }
 }
--- src/main/java/com/envelo/integrationapp/services/EventService.java
+++ src/main/java/com/envelo/integrationapp/services/EventService.java
@@ -1,12 +1,20 @@
 package com.envelo.integrationapp.services;
 
 import com.envelo.integrationapp.model.dtos.EventCreationDto;
+import com.envelo.integrationapp.model.dtos.info.EventDtoInfo;
+import com.envelo.integrationapp.model.dtos.info.EventParticipantDtoInfo;
+import com.envelo.integrationapp.model.dtos.info.EventPlaceDtoInfo;
 import com.envelo.integrationapp.model.entities.Event;
+import com.envelo.integrationapp.model.entities.EventParticipant;
+import com.envelo.integrationapp.model.enums.EventRole;
 import com.envelo.integrationapp.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -43,5 +51,23 @@ public class EventService {
         System.out.println(byId.getDeadlineDecision());
 
         return byId;
+    }
+    public List<EventDtoInfo> getAllUserCreatedEvents(Long userId, EventRole eventRole){
+        List<Event> events = eventRepository.findEventsByCreator(userId, eventRole);
+        List<EventDtoInfo> eventDtos = new ArrayList<>();
+        for (Event event : events) {
+            for (EventParticipant participant : event.getParticipants()) {
+                if (participant.getId() == userId) {
+                    EventDtoInfo eventDtoInfo = new EventDtoInfo();
+                    eventDtoInfo.setId(event.getId());
+                    eventDtoInfo.setTitle(event.getTitle());
+                    eventDtoInfo.setStartDate(event.getStartDate());
+                    eventDtoInfo.setEndDate(event.getEndDate());
+                    eventDtoInfo.setEventStatus(event.getEventStatus());
+                    eventDtos.add(eventDtoInfo);
+                }
+            }
+        }
+        return eventDtos;
     }
 }
