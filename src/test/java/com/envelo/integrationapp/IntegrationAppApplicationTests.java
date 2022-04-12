ae1066f7d53a34335d37dafe6a2239f4a618f552
Przemek Nosek <przemek.nosek@o2.pl>
2022-04-12 12:29:10 +0200

    init

---
+++ src/test/java/com/envelo/integrationapp/IntegrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envelo.integrationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
